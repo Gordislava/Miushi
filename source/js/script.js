a24c575faa38ea99a168b9846b5c578154a884d2
gordislava <gordislava@gmail.com>
2018-12-10 15:11:29 +0300

    header, promo-slider slick

---
+++ source/js/script.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+     $('.promo__slider-js').slick({
+         infinite: true,
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         autoplay: false,
+         dots: true,
+         arrows: true
+     });
+   });
+          
